a29c21cdcf20709deb3da21d3adc8c0415d594a4
Dmytro <dmytroshults@gmail.com>
2020-04-07 17:05:05 +0200

    pset7 SQL movies

---
+++ pset7/movies/13.sql
@@ -0,0 +1,7 @@
+--list the names of all people who starred in a movie in which Kevin Bacon also starred
+SELECT DISTINCT name FROM people 
+WHERE name != 'Kevin Bacon' AND id IN
+    (SELECT person_id FROM stars WHERE movie_id IN
+        (SELECT movie_id FROM stars WHERE person_id IN
+            (SELECT id FROM people WHERE name = 'Kevin Bacon' AND birth = 1958)))
+ORDER BY name

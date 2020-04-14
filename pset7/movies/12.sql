3512a64913b288326e69f14f6aa846957cdf5b6a
Dmytro <dmytroshults@gmail.com>
2020-04-14 21:01:02 +0200

    Updated

--- pset7/movies/12.sql
+++
@@ -1,7 +0,0 @@
---list the titles of all movies in which both Johnny Depp and Helena Bonham Carter starred
-SELECT title FROM movies
-JOIN stars ON stars.movie_id=movies.id
-JOIN people ON people.id=stars.person_id
-WHERE name = "Johnny Depp" OR name = "Helena Bonham Carter"
-GROUP BY title
-HAVING count(title) = 2
--- pset7/movies/9.sql
+++
@@ -1,7 +0,0 @@
---list the names of all people who starred in a movie released in 2004, ordered by birth year
-SELECT name FROM people
-JOIN stars ON stars.movie_id=people.id
-JOIN movies ON movies.id=stars.movie_id
-WHERE year=2004
-GROUP BY name
-ORDER BY birth
